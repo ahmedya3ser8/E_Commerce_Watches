ba4d0c6bb7cd973dcc277b0b7bcb30d0dd36dad2
=ahmedyasser <=ahmedya3ser8@gmail.com>
2023-07-30 07:22:41 +0300

    Creating Testimonials and New Sections

--- assets/js/main.js
+++ assets/js/main.js
@@ -33,3 +33,29 @@ window.addEventListener("scroll", () => {
     header.classList.remove("scroll-header");
   }
 });
+
+// Testimonials Swiper
+let testimonialSwiper = new Swiper('.testimonial-swiper', {
+  spaceBetween: 30,
+  loop: true,
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+});
+// New Swiper
+let newSwiper = new Swiper('.new-swiper', {
+  spaceBetween: 24,
+  loop: true,
+  breakpoints: {
+    576: {
+      slidesPerView: 2,
+    },
+    768: {
+      slidesPerView: 3,
+    },
+    1024: {
+      slidesPerView: 4,
+    },
+  },
+});
