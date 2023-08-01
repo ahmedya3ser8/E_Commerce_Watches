b9e538969b7b5a8167dd68bb6b555c7fa63dc05e
=ahmedyasser <=ahmedya3ser8@gmail.com>
2023-08-01 06:56:22 +0300

    Create Cart, Newsletter And Footer Sections

--- assets/js/main.js
+++ assets/js/main.js
@@ -59,3 +59,78 @@ let newSwiper = new Swiper('.new-swiper', {
     },
   },
 });
+
+// Scroll To Top
+let scrollUp = document.getElementById("scroll-up");
+let btn = document.querySelector(".scrollup-icon");
+
+window.addEventListener("scroll", () => {
+  if (window.scrollY >= 300) {
+    scrollUp.classList.add("show-scroll");
+  } else {
+    scrollUp.classList.remove("show-scroll");
+  }
+});
+
+btn.onclick = function() {
+  window.scrollTo({
+    top: 0,
+    behavior: "smooth",
+  });
+}
+
+// Active Link With Scroll
+let sections = document.querySelectorAll("section[id]");
+let navLink = document.querySelectorAll("header .nav-link");
+
+window.addEventListener("scroll", () => {
+  sections.forEach((section) => {
+    let id = section.getAttribute("id");
+    if (window.scrollY >= section.offsetTop - 150 && window.scrollY < section.offsetTop - 150 + section.offsetHeight) {
+      navLink.forEach((nav) => {
+        nav.classList.remove("active");
+        document.querySelector(`header .nav-link[href*= "${id}"]`).classList.add("active");
+      });
+    }
+  });
+});
+
+// Show Cart
+let cart = document.getElementById("cart"),
+  cartShop = document.getElementById("cart-shop"),
+  cartClose = document.getElementById("cart-close");
+
+if (cartShop) {
+  cartShop.addEventListener("click", () => {
+    cart.classList.add("show-cart");
+  });
+}
+
+if (cartClose) {
+  cartClose.addEventListener("click", () => {
+    cart.classList.remove("show-cart");
+  });
+}
+
+// DARK LIGHT THEME
+const themeButton = document.getElementById('theme-button');
+const darkTheme = 'dark-theme';
+const iconTheme = 'bx-sun';
+
+const selectedTheme = localStorage.getItem('selected-theme');
+const selectedIcon = localStorage.getItem('selected-icon');
+
+const getCurrentTheme = () => document.body.classList.contains(darkTheme) ? 'dark' : 'light';
+const getCurrentIcon = () => themeButton.classList.contains(iconTheme) ? 'bx bx-moon' : 'bx bx-sun';
+
+if (selectedTheme) {
+  document.body.classList[selectedTheme === 'dark' ? 'add' : 'remove'](darkTheme);
+  themeButton.classList[selectedIcon === 'bx bx-moon' ? 'add' : 'remove'](iconTheme);
+}
+
+themeButton.addEventListener('click', () => {
+  document.body.classList.toggle(darkTheme);
+  themeButton.classList.toggle(iconTheme);
+  localStorage.setItem('selected-theme', getCurrentTheme());
+  localStorage.setItem('selected-icon', getCurrentIcon());
+});
